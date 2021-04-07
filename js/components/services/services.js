7bd948c0074ae5da7caaf383e1128cced69fbbf9
Rimantas <belauzas@yahoo.com>
2021-04-07 13:19:29 +0300

    Services validation #21

--- js/components/services/services.js
+++ js/components/services/services.js
@@ -1,27 +1,43 @@
+import { isValidServices } from './isValidServices.js';
+import { isValidServiceItem } from './isValidServiceItem.js';
+
 function services(selector, data) {
     // input validation
+    if (!isValidServices(selector, data)) {
+        return false;
+    }
 
     // logic
     const DOM = document.querySelector(selector);
+    if (!DOM) {
+        console.error('Pagal pateikta selektoriu nerastas norimas elementas.');
+        return false;
+    }
+
     const { list, imgPath, maxCount } = data;
 
     let HTML = '';
+    let generatedServicesCount = 0;
 
     for (let i = 0; i < list.length; i++) {
         const service = list[i];
 
-        if (!service.active) {
+        if (!isValidServiceItem(service) ||
+            !service.active) {
             continue;
         }
+        if (generatedServicesCount === maxCount) {
+            break;
+        }
 
-        HTML += `<div class="col-12 col-md-6 col-lg-4 service-item">
+        generatedServicesCount++;
+        HTML += `<div class="service-item">
                     <img src="${imgPath + service.img}" alt="">
                     <h4>${service.title}</h4>
                     <p>${service.description}</p>
                     <a href="${service.link}" class="fa fa-long-arrow-right"></a>
                 </div>`;
     }
-
 
     // post logic validation
 
--- js/data/servicesData.js
+++ js/data/servicesData.js
@@ -3,6 +3,13 @@ const servicesData = {
     maxCount: 3,
     list: [
         {
+            img: 'mitech-box-image-style-01-image-03-100x108.png',
+            title: 'Data security',
+            description: 'Back up your database, store in a safe and secure place while still maintaining its accessibility.',
+            link: '#',
+            active: true
+        },
+        {
             img: 'mitech-box-image-style-01-image-01-100x108.png',
             title: 'IT design',
             description: 'We provide the most responsive and functional IT design for companies and businesses worldwide.',
@@ -15,6 +22,13 @@ const servicesData = {
             description: 'Some demo text',
             link: '#',
             active: false
+        },
+        {
+            img: 'mitech-box-image-style-01-image-02-100x108.png',
+            title: 'IT management',
+            description: 'It’s possible to simultaneously manage and transform information from one server to another.',
+            link: '#',
+            active: true
         },
         {
             img: 'mitech-box-image-style-01-image-02-100x108.png',
