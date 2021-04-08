81dd97d658b5ba0fdc60fdda2835414e23bddf4f
Rimantas <belauzas@yahoo.com>
2021-04-08 13:46:58 +0300

    Achievements

---
+++ js/data/achievementsData.js
@@ -0,0 +1,27 @@
+const achievementsData = {
+    maxCount: 4,
+    list: [
+        {
+            title: 'Successfully work with',
+            value: 1056,
+            subtitle: 'Happy clients',
+        },
+        {
+            title: 'Successfully completed',
+            value: 491,
+            subtitle: 'Finished projects',
+        },
+        {
+            title: 'Recriut more than',
+            value: 245,
+            subtitle: 'Skilled experts',
+        },
+        {
+            title: 'Blog update',
+            value: 1090,
+            subtitle: 'Media posts',
+        },
+    ]
+}
+
+export { achievementsData }
